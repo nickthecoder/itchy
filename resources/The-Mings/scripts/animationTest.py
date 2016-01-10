27ebbd0300e8b9031fd9f92f294e1db6ea0b74b1
Nick Robinson <nickthecoder@gmail.com>
2016-01-10 15:38:49 +0000

    Animation tests. Step through one frame at a time, on a keystroke.

--- resources/The-Mings/scripts/animationTest.py
+++ resources/The-Mings/scripts/animationTest.py
@@ -12,7 +12,19 @@ class AnimationTest(AbstractRole) :
         self.dx = 0
         self.dy = 0
         self.direction = 1
-                
+
+        self.key = Input.find("step")
+        self.wasDown = False
+
+    def animate(self):
+        if self.key.pressed() :
+            self.wasDown = True
+        else :
+            if self.wasDown :
+                AbstractRole.animate(self)
+                self.actor.moveBy( self.dx, self.dy )
+                self.wasDown = False
+
     def tick(self):
         self.actor.moveBy( self.dx, self.dy )
 
