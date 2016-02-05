fc9466d5662ca600bcd1ed3cd29486956da92066
Nick Robinson <nickthecoder@gmail.com>
2016-02-05 20:21:06 +0000

    Save Drunk-Invaders scenes using the new file format

---
+++ src/main/java/uk/co/nickthecoder/itchy/RedirectSceneDirector.java
@@ -0,0 +1,30 @@
+package uk.co.nickthecoder.itchy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.co.nickthecoder.itchy.property.Property;
+import uk.co.nickthecoder.itchy.property.StringProperty;
+
+public class RedirectSceneDirector extends PlainSceneDirector
+{
+    protected static final List<Property<SceneDirector, ?>> properties = new ArrayList<Property<SceneDirector, ?>>();
+    
+    static {
+        properties.add( new StringProperty<SceneDirector>( "sceneName").aliases("redirectSceneName") );
+    }
+
+    public String sceneName = "";
+    
+    @Override
+    public List<Property<SceneDirector, ?>> getProperties()
+    {
+        return properties;
+    }
+
+    @Override
+    public void onLoaded()
+    {
+        Itchy.getGame().getDirector().startScene( this.sceneName );
+    }
+}
