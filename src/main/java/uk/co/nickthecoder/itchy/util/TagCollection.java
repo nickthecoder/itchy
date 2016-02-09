bddcbb058e939290becaebf560dbdf3435057bf6
Nick Robinson <nickthecoder@gmail.com>
2016-02-09 11:39:32 +0000

    Remove unwanted code missed by the previous commit. Oops.

--- src/main/java/uk/co/nickthecoder/itchy/util/TagCollection.java
+++ src/main/java/uk/co/nickthecoder/itchy/util/TagCollection.java
@@ -7,7 +7,6 @@ package uk.co.nickthecoder.itchy.util;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class TagCollection<M>
@@ -38,45 +37,6 @@ public class TagCollection<M>
         return new HashSet<M>(result);
     }
 
-    /**
-     * Iterate over all members that have any of the tags listed. Each member should be included only once.
-     * @param tags
-     * @return
-     */
-    public Iterator<M> iterateTagMembers( String... tags )
-    {
-        if (tags.length == 0) {
-            return new NullIterator<M>();
-        } else if (tags.length == 1) {
-            return getTagMembers( tags[0] ).iterator();
-        } else {
-            CompoundIterator<M> result = new CompoundIterator<M>();
-            result.add( getTagMembers( tags[0] ).iterator() );
-            
-            for ( int i = 1; i < tags.length; i ++ ) {
-                Iterator<M> oneTag = getTagMembers( tags[i] ).iterator();
-                // We cannot just join them together, because we could get duplicates, so we need to filter out
-                // any members that have any tags earlier in the list.
-                Filter<M> removeDuplicates = removeDuplicatesFilter( tags, i-1 );
-                
-                result.add( new FilteredIterator<M>( oneTag, removeDuplicates ) );
-            }
-            return result;
-        }
-    }
-    
-    private Filter<M> removeDuplicatesFilter( String[] tags, int n )
-    {
-        if ( n == 0 ) {
-            return new WithoutTagFilter<M>( tags[0] );
-        } else {
-            CompoundFilter<M> result = new AndFilter<M>();
-            for ( int i = 0; i < n; i ++ ) {
-                result.add( new WithoutTagFilter<M>( tags[ i ] ) );
-            }
-            return result;
-        }
-    }
     
     public void remove( String tag, M member )
     {
