983d1c2ae6da85980d1837b41d3877865dbfa085
Nick Robinson <nickthecoder@gmail.com>
2016-03-03 01:55:55 +0000

    Fix 1st frame of rocket's jet animation (projectiles of an explosion).

--- resources/Cavern-Quest/scripts/rocket.py
+++ resources/Cavern-Quest/scripts/rocket.py
@@ -10,7 +10,7 @@ class Rocket(GridRole) :
         invader.deathEvent( "launch" )
         ExplosionBuilder(self.actor) \
             .dependent().forever().follow().offset(0,20).projectilesPerTick(3) \
-            .zOrder(10).spread(-65,-105).distance(10).randomSpread().speed(1,2,0,0).fade(3).eventName("flame") \
+            .zOrder(10).spread(-70,-110).distance(10).randomSpread().speed(1,2,0,0).fade(3).eventName("flame") \
             .create()
             
     # Boiler plate code - no need to change this
