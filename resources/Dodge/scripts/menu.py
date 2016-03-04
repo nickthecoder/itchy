e1d429b41484239d4bdb929cd0ec05343ae364ed
Nick Robinson <nickthecoder@gmail.com>
2016-03-04 23:36:14 +0000

    Dodge 'Em menu now has thumbnails of the scenes (which are auto generated)

--- resources/Dodge/scripts/menu.py
+++ resources/Dodge/scripts/menu.py
@@ -6,13 +6,47 @@ class Menu(PlainSceneDirector) :
 
     def __init__(self) :
         self.playing = False
+        
+        
+    def onLoaded(self) :
+        self.setSceneName( game.preferences.get("scene", "a") )
 
+
+    def setSceneName(self, sceneName) :
+        self.sceneName = sceneName
+        game.findActorById("thumbnail").event( "unknown" )
+        game.findActorById("thumbnail").event( sceneName )
+        
+        game.findActorById("name").appearance.pose.text = sceneName
+
+        
     def onKeyDown(self, event) :
         # Press a letter to start the scene.
         if game.hasScene( event.c ) :
             game.director.startScene( event.c )
     
     
+    def onMessage(self, message) :
+        
+        if message == "go" :
+            game.startScene( self.sceneName )
+            
+        if message == "next" :
+            self.changeScene( 1 )
+
+        if message == "prev" :
+            self.changeScene( -1 )
+
+    
+    def changeScene( self, delta ) :
+        sceneName = chr( ord( self.sceneName ) + delta )
+        if game.resources.getScene( sceneName ) is None :
+            return
+
+        self.setSceneName( sceneName )
+        game.preferences.put("scene", sceneName)
+
+
     # Boiler plate code - no need to change this
     def getProperties(self):
         return properties
